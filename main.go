f3d9bcf7ca700aef06436dbefcce13c8cb2ac05c
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-12-03 07:55:43 -0300

    Cleaned some code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -20,41 +19,16 @@ func main() {
     apiCfg := &apiConfig{}
     mux.Handle(
         "/app/", 
-        apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))),
+        apiCfg.middlewareMetrics(http.StripPrefix("/app", http.FileServer(http.Dir(".")))),
     )
     mux.HandleFunc("GET /api/healthz", handleResponse)
-    mux.HandleFunc("GET /api/metrics", apiCfg.handleNumberOfServerHits)
-    mux.HandleFunc("POST /api/reset", apiCfg.handleResetServerHits)
+    mux.HandleFunc("POST /api/validate_chirp", handleChirpValidation)
+    mux.HandleFunc("GET /admin/metrics", apiCfg.handleNumberOfServerHits)
+    mux.HandleFunc("POST /admin/reset", apiCfg.handleResetServerHits)
     err:=server.ListenAndServe()
     if err != nil {
         log.Fatal(err)
     }
 }
 
-func handleResponse(w http.ResponseWriter, _ *http.Request) {
 
-    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(200)
-    w.Write([]byte("OK"))
-}
-
-func (cfg *apiConfig) handleNumberOfServerHits(w http.ResponseWriter, _ *http.Request) {
-    fmt.Println("Hits: ", cfg.fileserverHits.Load())
-    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(200)
-    w.Write([]byte(fmt.Sprint("Hits: ", cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) handleResetServerHits(w http.ResponseWriter, _ *http.Request) {
-    cfg.fileserverHits.Store(0)
-    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(200)
-    w.Write([]byte("OK"))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        next.ServeHTTP(w, r)
-        cfg.fileserverHits.Add(1)
-    })
-}
